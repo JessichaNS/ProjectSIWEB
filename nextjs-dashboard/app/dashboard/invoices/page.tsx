27a94d5107de11e9152ce117b6fdd4d61b0fc121
JessichaNS <jessichanovranisipayung@gmail.com>
2026-05-17 00:14:59 +0700

    akhirnya

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,6 +1,6 @@
-import Pagination from "@/app/ui/invoices/pagination";
+import Pagination from './pagination';
+import Table from './table';
 import Search from "@/app/ui/search";
-import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
@@ -17,6 +17,8 @@ export default async function Page(props: {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
+
+  console.log("Cek Total Pages:", totalPages);
 
   return (
     <div className="w-full">
